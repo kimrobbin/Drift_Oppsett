d4e1688d043894c55b9c09e23cb16468444a2bf8
kimrobbin <kimrobbinkf@gmail.com>
2025-03-17 11:41:23 +0100

    Update windowserver.ps1

--- windowserver.ps1
+++ windowserver.ps1
@@ -1,31 +1,31 @@
-$computername = "MainSever"
-
-$ip = "10.100.100.100"
-$gateway = "10.0.0.1"
+# Define variables
+$computername = "Main"
+$ip = "192.168.35.1"
+$gateway = "192.168.35.1"
 $length = 24
+$vlanID = 335
 
 $domain1 = "kimrobbin"
 $domain2 = "local"
 $domain = "$domain1.$domain2"
-$ouName = "Elever"
-$ouName2 = "Lærere"
+$ouName = "users"
 
+# Rename Computer
 Rename-Computer -NewName $computername -Restart
 
+# Set Static IP Address
 New-NetIPAddress -IPAddress $ip -PrefixLength $length -DefaultGateway $gateway
 
-$features = @(
-    "AD-Domain-Services",
-    "DHCP",
-    "DNS",
-    "Hyper-V"
-)
+# Configure VLAN
+Set-NetAdapter -Name "Ethernet" -VlanID $vlanID
 
+# Install required features
+$features = @("AD-Domain-Services", "DHCP", "DNS", "Hyper-V")
 Install-WindowsFeature -Name $features -IncludeAllSubFeature -IncludeManagementTools -Restart
 
+# Configure Active Directory Domain Services
 Install-ADDSForest -DomainName $domain -InstallDNS
 
+# Create Organizational Unit
 New-ADOrganizationalUnit -Name $ouName -Path "DC=$domain1,DC=$domain2"
-New-ADOrganizationalUnit -Name $ouName2 -Path "DC=$domain1,DC=$domain2"
-
-
+Set-ADDomain -Identity $domain -DefaultUserContainer "OU=$ouName,DC=$domain1,DC=$domain2"
