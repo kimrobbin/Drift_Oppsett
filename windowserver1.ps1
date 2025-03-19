e2d89cd70006b5b2d047960040f260114ba8c1c9
kimrobbin <kimrobbinkf@gmail.com>
2025-03-19 10:33:28 +0100

    logging

--- windowserver1.ps1
+++ windowserver1.ps1
@@ -1,3 +1,6 @@
+# Start logging
+Start-Transcript -Path "C:\Logs\windowserver2.log"
+
 # Define variables
 $computername = "MainServer"
 $ip = "192.168.35.1"
@@ -19,11 +22,51 @@ $startRange = "192.168.35.100"
 $endRange = "192.168.35.200"
 $subnetMask = "255.255.255.0"
 
-# Rename Computer
-Rename-Computer -NewName $computername 
+# Set Static IP Address
+try {
+    New-NetIPAddress -IPAddress $ip -PrefixLength $length -DefaultGateway $gateway -InterfaceAlias $netAdapterName
+} catch {
+    Write-Error "Failed to add IP address: $_"
+}
 
-$features = @("AD-Domain-Services", "DHCP", "DNS", "Hyper-V")
-Install-WindowsFeature -Name $features -IncludeAllSubFeature -IncludeManagementTools -NoRestart
+# Set DNS Server
+try {
+    Set-DnsClientServerAddress -InterfaceAlias $netAdapterName -ServerAddresses ("8.8.8.8", "8.8.4.4")
+} catch {
+    Write-Error "Failed to set DNS server address: $_"
+}
 
-# Restart computer to apply changes
-Restart-Computer -Force
+# Create a new virtual switch with an external connection type
+try {
+    New-VMSwitch -Name $vSwitchName -NetAdapterName $netAdapterName -AllowManagementOS $true
+} catch {
+    Write-Error "Failed to create virtual switch: $_"
+}
+
+# Set VLAN ID for the virtual switch
+try {
+    Set-VMNetworkAdapterVlan -AllowManagementOS -VMNetworkAdapterName $netAdapterName -Access -VlanId $vlanID
+} catch {
+    Write-Error "Failed to set VLAN ID: $_"
+}
+
+# Configure Active Directory Domain Services
+Install-ADDSForest -DomainName $domain -InstallDNS
+
+# Create Organizational Unit
+New-ADOrganizationalUnit -Name $ouName -Path "DC=$domain1,DC=$domain2"
+Set-ADDomain -Identity $domain -DefaultUserContainer "OU=$ouName,DC=$domain1,DC=$domain2"
+
+# Configure DHCP Server
+Add-DhcpServerv4Scope -Name $scopeName -StartRange $startRange -EndRange $endRange -SubnetMask $subnetMask
+Set-DhcpServerv4OptionValue -ScopeId $ip -Router $gateway -DnsServer $ip
+
+# Authorize DHCP Server in Active Directory
+try {
+    Add-DhcpServerInDC -DnsName $computername -IPAddress $ip
+} catch {
+    Write-Error "Failed to authorize DHCP server: $_"
+}
+
+# Stop logging
+Stop-Transcript
